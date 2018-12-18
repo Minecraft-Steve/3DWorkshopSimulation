0c71a6b4676c6677996d04ad80da25b0c505aef3
Minecraft-Steve <2695676562@qq.com>
2018-12-18 19:40:08 +0800

    人物点击移动动画已实现、摄像机跟随（未实现旋转）

--- Unity Project/Assets/_Script/RadiationDetection.cs
+++ Unity Project/Assets/_Script/RadiationDetection.cs
@@ -13,8 +13,18 @@ public class RadiationDetection : MonoBehaviour
     //设置射线在plane上的目标点target
     private Vector3 target;
 
+    /// <summary>
+    /// 主摄像机
+    /// </summary>
     public GameObject camera;
+    /// <summary>
+    /// 物体与摄像机之间的偏移量
+    /// </summary>
     private Vector3 offset;
+    /// <summary>
+    /// 角色的Animator组件
+    /// </summary>
+    public Animator playerAnimator;
 
     private void Start()
     {
@@ -37,11 +47,13 @@ public class RadiationDetection : MonoBehaviour
                 //Debug.Log("坐标为：" + hit.point);
                 //检测到碰撞，将检测到的点记录下来
                 target = hit.point;
+                
             }
+            cube.transform.LookAt(target);
+            Move(target);    
         }
-
-        Move(target);
         camera.gameObject.transform.position = cube.position + offset;
+        
     }
 
     /// <summary>
@@ -50,7 +62,16 @@ public class RadiationDetection : MonoBehaviour
     /// <param name="target"></param>
     void Move(Vector3 target)
     {
-        cube.position = Vector3.Lerp(cube.position, new Vector3(target.x, cube.position.y, target.z), Time.deltaTime);
-        //cube.DOMove(target, 1f);
+        //开启运动动画
+        playerAnimator.SetBool("IsRun", true);
+        //cube.position = Vector3.Lerp(cube.position, new Vector3(target.x, cube.position.y, target.z), Time.deltaTime);
+        cube.DOMove(target, 1f).OnComplete(Stop);
+    }
+
+    void Stop()
+    {
+        //关闭运动动画
+        playerAnimator.SetBool("IsRun", false);
+           
     }
 }
