d1d0d023371a295f070a31a882bbc5e1b0f0c836
Dawn-and-dusk <m17862576179@163.com>
2018-12-20 23:05:03 +0800

    摄像机旋转

---
+++ Unity Project/Assets/_Script/CameraControl.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraControl : MonoBehaviour
+{
+    /// <summary>
+    /// 摄像机移动速率
+    /// </summary>
+    public float speed;
+
+    /// <summary>
+    /// 摄像机和人物之间的距离
+    /// </summary>
+    Vector3 distance;
+
+    /// <summary>
+    /// 记录摄像机和人物之间的距离
+    /// </summary>
+    float offset;
+
+    /// <summary>
+    /// 目标人物
+    /// </summary>
+    public Transform figure;
+
+    /// <summary>
+    /// 最大缩放值
+    /// </summary>
+    public float maxScroll;
+
+    /// <summary>
+    /// 最小缩放值
+    /// </summary>
+    public float minScroll;
+
+    /// <summary>
+    ///  Y轴最大旋转角度
+    /// </summary>
+    public float maxRotationAngleY;
+
+    /// <summary>
+    /// Y轴最小旋转角度
+    /// </summary>
+    public float minRotationAngleY;
+
+    private void Start()
+    {
+        //计算摄像机和人物之间的距离
+        distance = transform.position - figure.position;
+    }
+
+    void Update()
+    {
+        CameraFollow();
+        ScrollView();
+        Rotate();
+    }
+
+    /// <summary>
+    /// 摄像机跟随
+    /// </summary>
+    void CameraFollow()
+    {
+        transform.position = figure.transform.position + distance;
+    }
+
+    /// <summary>
+    /// 缩放方法
+    /// </summary>
+    void ScrollView()
+    {
+        //判断滚轮的滑动和缩放的大小
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && distance.magnitude < maxScroll)
+        {
+            Debug.Log("拉远");
+            //将三维坐标转化为具体数
+            offset = distance.magnitude;
+            //计算经过滚轮滑动后的距离
+            offset += System.Math.Abs(Input.GetAxis("Mouse ScrollWheel")) * speed;
+            //判断所在距离是否超过限定的距离，然后更改
+            offset = (offset >= maxScroll) ? maxScroll - 1 : offset;
+            //将距离变成三维坐标
+            distance = distance.normalized * offset;
+        }
+        //判断滚轮的滑动和缩放的大小
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && distance.magnitude > minScroll)
+        {
+            Debug.Log("拉近");
+            //将三维坐标转化为具体数
+            offset = distance.magnitude;
+            //计算经过滚轮滑动后的距离
+            offset += -System.Math.Abs(Input.GetAxis("Mouse ScrollWheel")) * speed;
+            //判断所在距离是否超过限定的距离，然后更改
+            offset = (offset <= minScroll) ? offset + 1 : offset;
+            //将距离变成三维坐标
+            distance = distance.normalized * offset;
+        }
+    }
+
+    /// <summary>
+    /// 旋转方法
+    /// </summary>
+    void Rotate()
+    {
+        //判断鼠标点击是否为该按键
+        if (Input.GetMouseButton(0))
+        {
+            //通过鼠标的滑动来计算在哪个轴来尽行旋转
+            transform.RotateAround(figure.position, figure.up, Input.GetAxis("Mouse X") * speed);
+            //计算摄像机和人物之间的坐标差
+            distance = transform.position - figure.position;
+            //更新摄像机的位置
+            transform.position = figure.position + distance;
+
+            //最大限度
+            //在Y轴的旋转是否超过限度
+            if (transform.localEulerAngles.x < maxRotationAngleY && Input.GetAxis("Mouse Y") < 0)
+            {
+                //通过鼠标的滑动来计算在哪个轴来尽行旋转
+                transform.RotateAround(figure.position, transform.right, -Input.GetAxis("Mouse Y") * speed);
+                //计算摄像机和人物之间的坐标差
+                distance = transform.position - figure.position;
+                //更新摄像机的位置
+                transform.position = figure.position + distance;
+            }
+            //最小限度
+            //在Y轴的旋转是否超过限度
+            if (transform.localEulerAngles.x > minRotationAngleY && Input.GetAxis("Mouse Y") > 0)
+            {
+                //通过鼠标的滑动来计算在哪个轴来尽行旋转
+                transform.RotateAround(figure.position, figure.right, -Input.GetAxis("Mouse Y") * speed);
+                //计算摄像机和人物之间的坐标差
+                distance = transform.position - figure.position;
+                //更新摄像机的位置
+                transform.position = figure.position + distance;
+            }
+        }
+    }
+}
---
+++ Unity Project/Assets/_Script/RadiographicInspection.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+/// <summary>
+/// 射线检测
+/// </summary>
+public class RadiographicInspection : MonoBehaviour
+{
+    /// <summary>
+    /// 射线的地面的的碰撞点
+    /// </summary>
+    Vector3 targetPos;
+
+    /// <summary>
+    /// 动画组件
+    /// </summary>
+    public Animator animator;
+
+    private void Update()
+    {
+        //判断鼠标检测的是否是右键
+        if (Input.GetMouseButtonDown(1))
+        {
+            //从鼠标当前发射一条射线
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //保存射线检测的结果
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                //获得射线和地面的碰撞点
+                targetPos = new Vector3(raycastHit.point.x, transform.position.y, raycastHit.point.z);
+                //使人物朝向鼠标点击的位置
+                transform.LookAt(targetPos);
+                //播放动画
+                Switchover();
+                //移动
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, 2f * Time.deltaTime);
+            }
+        }
+
+        //float dis = (transform.position - targetPos).sqrMagnitude;
+        //Debug.Log(dis);
+        //判断人物到目标点的距离
+        if ((int)((transform.position - targetPos).sqrMagnitude)<=1)
+        {
+            Debug.Log("停止");
+            //停止动画播放
+            Stop();
+        }
+    }
+
+    /// <summary>
+    /// 开始播放动画
+    /// </summary>
+    public void Switchover()
+    {
+        Debug.Log("动画播放");
+        animator.SetBool("QieHuan", true);
+    }
+
+    /// <summary>
+    /// 停止动画
+    /// </summary>
+    public void Stop()
+    {
+        Debug.Log("动画停止");
+        animator.SetBool("QieHuan", false);
+    }
+}
